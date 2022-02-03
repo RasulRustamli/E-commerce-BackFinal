0523a24bebe95e55b92d75fdbaaa5d00bf8de2bf
RasulRustamli <rasular@code.edu.az>
2022-02-04 03:36:36 +0400

    feat:create Footer Component

---
+++ E-commerce-BackFinal/Controllers/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using E_commerce_BackFinal.DAL;
+using E_commerce_BackFinal.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commerce_BackFinal.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly Context _context;
+
+
+        public ContactController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ContactUS contact = _context.Contacts.FirstOrDefault();
+            List<CompanySlider> companySliders = _context.CompanySliders.ToList();
+            List<Service> services = _context.Services.ToList();
+            ViewData["CompanySliders"] = companySliders;
+            ViewData["Services"] = services;
+            return View(contact);
+        }
+    }
+}
---
+++ E-commerce-BackFinal/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using E_commerce_BackFinal.DAL;
+using E_commerce_BackFinal.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commerce_BackFinal.ViewComponents
+{
+    public class FooterViewComponent:ViewComponent
+    {
+        private readonly Context _context;
+        public FooterViewComponent(Context context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ContactUS contact = _context.Contacts.FirstOrDefault();
+            return View(await Task.FromResult(contact));
+        }
+    }
+}
